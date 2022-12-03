f838676b57ee4205092ad99f8a3ff801fef95b2b
Wolter Smit <w.smit@topdesk.com>
2022-12-03 15:07:38 +0100

    Day 3

---
+++ day3/day3-2.swift
@@ -0,0 +1,17 @@
+var lines : [String] = []
+
+while let line = readLine() {
+    lines.append(line)
+}
+
+let groups = stride(from: 0, to: lines.count, by: 3)
+                .map { Array(lines[$0..<$0+3]) }
+                .map { $0.map { Set($0) } }
+
+let badge = groups
+            .compactMap { group in group[0].intersection(group[1].intersection(group[2])).first?.asciiValue }
+            .map { ascii in Int(ascii>96 ? ascii-96 : ascii-64+26) }
+            .reduce(0,+)
+
+print(badge)
+
