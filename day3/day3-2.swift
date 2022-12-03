d0ee64bba2e77a9376a8fbcc916886e24336dde2
Wolter Smit <w.smit@topdesk.com>
2022-12-03 15:28:08 +0100

    Even shorter

--- day3/day3-2.swift
+++ day3/day3-2.swift
@@ -5,8 +5,7 @@ while let line = readLine() {
 }
 
 let groups = stride(from: 0, to: lines.count, by: 3)
-                .map { Array(lines[$0..<$0+3]) }
-                .map { $0.map { Set($0) } }
+                .map { lines[$0..<$0+3].map { Set($0)} }
 
 let badge = groups
             .compactMap { group in group[0].intersection(group[1].intersection(group[2])).first?.asciiValue }
